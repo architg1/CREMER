1294c34824a477ef256c8fec92e5ff8d55ef26ed
architg1 <ag18052002@gmail.com>
2022-07-13 17:03:12 +0800

    fixed some bugs

--- seu_detection.c
+++ seu_detection.c
@@ -4,23 +4,24 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int numThreads = 10; // no. of threads will be adjusted based on processor capability and available RAM to maximise detection
+#define numThreads 10 // no. of threads will be adjusted based on processor capability and available RAM to maximise detection
+int result[numThreads];
 
 void *factorial_10(void* idp){
     result[(int)idp] = 10*9*8*7*6*4*3*2;
+    pthread_exit(NULL);
 }
 
-void create_threads(){
+void detect_seu(){
+
     pthread_t factThread[numThreads];
-    int result[numThreads];
     int ids[numThreads];
+
     for(int tCnt = 0; tCnt < numThreads; tCnt++){
         ids[tCnt] = tCnt;
         pthread_create(&factThread[tCnt], NULL, factorial_10, &ids[tCnt]);
     }
-}
 
-void detect_seu(){
     for(int tCnt = 0; tCnt < numThreads; tCnt++){
         if(result[tCnt]!=3628800){
             printf("SEU detected!");
